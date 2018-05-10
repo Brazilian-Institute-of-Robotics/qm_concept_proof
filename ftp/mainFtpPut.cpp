0aa8bd2e6137278e76559a1005d16a0484f417a0
rodrigopvb <rodrigopaixaovb@gmail.com>
2018-05-10 15:55:06 -0300

    add qm concept proof rosserial windows and ftp

---
+++ ftp/mainFtpPut.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+std::string username = "ftpuser";
+std::string password = "geo011";
+std::string urlFtp = " 192.168.0.1 ";
+
+void executeShellScript(const std::string& directory){
+
+std::string initialConnection = "ncftpput -R -W 'mkdir /" + directory + "' -v -u" 
++ username + " -p " + password + urlFtp + " / ~/Documents/" + directory;
+
+system(initialConnection.c_str());
+}
+
+int main() {
+
+executeShellScript("teste");
+
+return 0;
+}
